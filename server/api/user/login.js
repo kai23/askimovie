a25de5701b6ba2700414a6fecdda60f9f07dadc6
Florian <florian.chevallier03@gmail.com>
2017-05-18 23:04:04 +0200

    reste à faire l'API plex

--- server/api/user/login.js
+++ server/api/user/login.js
@@ -1,4 +1,13 @@
-module.exports = (req, res, next) => {
-  console.log('req.body', req.body);
-  res.send({ status: 'OK' });
+function loggedWithPlex() {
+  return false;
+}
+
+
+module.exports = (req, res) => {
+  const loggedWithPlexResponse = loggedWithPlex();
+  if (loggedWithPlexResponse) {
+    res.send({ status: 'OK' });
+  } else {
+    res.send({ status: 'KO' });
+  }
 };
