35dd10ffea8dbc618409b0351b1a8f8fc9a630c8
Florian <florian.chevallier03@gmail.com>
2017-09-04 09:27:05 +0200

    change gitignore

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const prefix = require('./config.json').prefix;
 const userLogin = require('./api/user/login.js');
 const userSession = require('./api/user/session.js');
+const mediaSearch = require('./api/media/search.js');
 
 module.exports = [{
   path: `${prefix}/v1.0/user/login`,
@@ -9,5 +10,9 @@ module.exports = [{
 }, {
   path: `${prefix}/v1.0/user/session`,
   action: userSession,
+  method: 'GET',
+}, {
+  path: `${prefix}/v1.0/media/search`,
+  action: mediaSearch,
   method: 'GET',
 }];
